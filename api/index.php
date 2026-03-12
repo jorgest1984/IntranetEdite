5ff9dceb057ebc7ea6a7f47e7b5ad9b259c0a24c
Jorge Salguero <sisqogr@gmail.com>
2026-03-12 10:19:57 +0100

    Deploy definitively with user email attached

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 // Enrutador Front Controller para Vercel
 // Forzando un nuevo despliegue en Vercel tras actualizar a Pro
+// Con el nuevo correo electronico
 // Esto permite a Vercel tener su archivo dentro de la carpeta obligatoria "api"
 // pero ejecutar el código del directorio raíz para no romper las rutas de los includes locals.
 
