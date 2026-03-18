e6c59d356b616562333d02fba24f23160e391eb4
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-03-18 11:35:13 +0100

    fix: update Aula Virtual URL

--- home.php
+++ home.php
@@ -38,7 +38,7 @@ $sections = [
         ['title' => 'FTP NAS Backup', 'icon' => '<svg viewBox="0 0 24 24"><path d="M15 13l-4 4-4-4h3V4h2v9h3zm4-2v6H5v-6H3v8h18v-8h-2z"/></svg>', 'url' => '#', 'color' => 'red', 'external' => true],
         ['title' => 'Sistema de tickets', 'icon' => '<svg viewBox="0 0 24 24"><path d="M12 1.95c-5.52 0-10 4.48-10 10s4.48 10 10 10h5v-2h-5c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8v1.43c0 .79-.71 1.57-1.5 1.57s-1.5-.78-1.5-1.57v-1.43c0-2.76-2.24-5-5-5s-5 2.24-5 5 2.24 5 5 5c1.38 0 2.64-.56 3.54-1.47.65.89 1.77 1.47 2.96 1.47 1.97 0 3.5-1.53 3.5-3.5v-1.43c0-5.52-4.48-10-10-10zm0 13c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"/></svg>', 'url' => '#', 'color' => 'red', 'external' => true],
         ['title' => 'Wiki', 'icon' => '<svg viewBox="0 0 24 24"><path d="M12 2L1 21h22L12 2zm1 14h-2v-2h2v2zm0-4h-2V8h2v4z"/></svg>', 'url' => '#', 'color' => 'red', 'external' => true], // Cambiado a un ícono default
-        ['title' => 'Aula Virtual', 'icon' => '<svg viewBox="0 0 24 24"><path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2.12-1.15V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72l5 2.73 5-2.73v3.72z"/></svg>', 'url' => '#', 'color' => 'red', 'external' => true]
+        ['title' => 'Aula Virtual', 'icon' => '<svg viewBox="0 0 24 24"><path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2.12-1.15V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72l5 2.73 5-2.73v3.72z"/></svg>', 'url' => 'https://aulavirtual.grupoefp.es/', 'color' => 'red', 'external' => true]
     ]
 ];
 ?>
