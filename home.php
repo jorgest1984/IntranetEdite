8f80e39e4f436cf2df1ba861bfe2cbcb776a78fd
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-04-10 09:49:22 +0200

    UI: Linked Moodle Courses stat card to cursos.php

--- home.php
+++ home.php
@@ -121,7 +121,7 @@ $sections = [
                 </div>
             </a>
             
-            <div class="stat-card">
+            <a href="cursos.php" class="stat-card">
                 <div class="stat-icon success">
                     <svg viewBox="0 0 24 24"><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z"/></svg>
                 </div>
@@ -129,7 +129,7 @@ $sections = [
                     <div class="stat-value"><?= $stats['cursos_moodle'] ?></div>
                     <div class="stat-label">Cursos Moodle</div>
                 </div>
-            </div>
+            </a>
             
             <div class="stat-card">
                 <div class="stat-icon warning">
