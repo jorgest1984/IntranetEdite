a86c7c339746392cf71ae1e258c2b37e9f6885ce
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-04-10 14:50:41 +0200

    UI: Refinado diseño de Formación Bonificada (iconos más pequeños y sin tono verde)

--- formacion_bonificada.php
+++ formacion_bonificada.php
@@ -7,17 +7,17 @@ $sections = [
     [
         'title' => 'Empresas',
         'url' => 'buscar_empresas.php',
-        'icon' => '<svg viewBox="0 0 24 24"><path d="M2 22h20V10L12 2 2 10v12zm2-2v-9l8-6.4L20 11v9H4zM7 15h2v2H7v-2zm0-4h2v2H7v-2zm4 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-4 4h2v2h-2v-2zm4 0h2v2h-2v-2z" fill="white"/></svg>'
+        'icon' => '<svg viewBox="0 0 24 24"><path d="M2 22h20V10L12 2 2 10v12zm2-2v-9l8-6.4L20 11v9H4zM7 15h2v2H7v-2zm0-4h2v2H7v-2zm4 0h2v2h-2v-2zm4 0h2v2h-2v-2zm-4 4h2v2h-2v-2zm4 0h2v2h-2v-2z"/></svg>'
     ],
     [
         'title' => 'Trabajadores',
         'url' => 'buscar_alumnos.php',
-        'icon' => '<svg viewBox="0 0 24 24"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" fill="white"/></svg>'
+        'icon' => '<svg viewBox="0 0 24 24"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/></svg>'
     ],
     [
         'title' => 'Acciones Formativas',
         'url' => 'acciones_formativas.php',
-        'icon' => '<svg viewBox="0 0 24 24"><path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72l5 2.73 5-2.73v3.72z" fill="white"/></svg>'
+        'icon' => '<svg viewBox="0 0 24 24"><path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72l5 2.73 5-2.73v3.72z"/></svg>'
     ]
 ];
 ?>
