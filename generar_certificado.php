4391d597eedd61b3f0ac0b1d8a52baa089ae68e9
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-04-22 14:27:04 +0200

    Fix: Removed undefined ROLE_COORD constant from certificate generator.

--- generar_certificado.php
+++ generar_certificado.php
@@ -3,7 +3,7 @@
 require_once 'includes/auth.php';
 require_once 'includes/config.php';
 
-if (!has_permission([ROLE_ADMIN, ROLE_COORD])) {
+if (!has_permission([ROLE_ADMIN])) {
     die("Acceso denegado.");
 }
 
