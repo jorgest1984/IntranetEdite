bee771f8c23d0fc2134cb078c026ad46f754aa18
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-06-19 11:21:19 +0200

    Test moodle tables

--- test_moodle2.php
+++ test_moodle2.php
@@ -18,33 +18,11 @@ try {
     $prop->setAccessible(true);
     $mpdo = $prop->getValue($moodleDb);
 
-    $courseMoodleId = 2; 
+    $stmtMod = $mpdo->prepare("SHOW TABLES");
+    $stmtMod->execute();
+    $tables = $stmtMod->fetchAll(PDO::FETCH_COLUMN);
 
-    $stmt = $pdo->prepare("SELECT c.moodle_id as curso_moodle_id, af.id_plataforma FROM acciones_formativas af JOIN cursos c ON af.curso_id = c.id WHERE af.id = 12");
-    $stmt->execute();
-    $af = $stmt->fetch();
-    if ($af) {
-        $courseMoodleId = $af['curso_moodle_id'] ?: $af['id_plataforma'];
-    }
-
-    $sqlModules = "SELECT cm.id as coursemoduleid, cm.section, 
-                         COALESCE(a.name, p.name, r.name, q.name, f.name, b.name, s.name, 'Actividad') as name
-                  FROM mdl_course_modules cm
-                  JOIN mdl_modules m ON cm.module = m.id
-                  LEFT JOIN mdl_assign a ON m.name = 'assign' AND cm.instance = a.id
-                  LEFT JOIN mdl_page p ON m.name = 'page' AND cm.instance = p.id
-                  LEFT JOIN mdl_resource r ON m.name = 'resource' AND cm.instance = r.id
-                  LEFT JOIN mdl_quiz q ON m.name = 'quiz' AND cm.instance = q.id
-                  LEFT JOIN mdl_forum f ON m.name = 'forum' AND cm.instance = f.id
-                  LEFT JOIN mdl_book b ON m.name = 'book' AND cm.instance = b.id
-                  LEFT JOIN mdl_scorm s ON m.name = 'scorm' AND cm.instance = s.id
-                  WHERE cm.course = ?";
-
-    $stmtMod = $mpdo->prepare($sqlModules);
-    $stmtMod->execute([$courseMoodleId]);
-    $moduleRows = $stmtMod->fetchAll(PDO::FETCH_ASSOC);
-
-    echo json_encode(['cmid' => $courseMoodleId, 'modules' => $moduleRows]);
+    echo json_encode(['tables' => $tables]);
 } catch (Throwable $e) {
     echo json_encode(['error' => 'Exception', 'msg' => $e->getMessage()]);
 }
