cba0167ed99f814cf673bf1f91d524e7be900d79
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-06-23 13:24:50 +0200

    Update dump_grupos to dump alumnos

--- dump_grupos.php
+++ dump_grupos.php
@@ -1,8 +1,1 @@
-<?php
-require_once 'includes/config.php';
-try {
-    $stmt = $pdo->query("DESCRIBE grupos");
-    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC), JSON_PRETTY_PRINT);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+<?php require 'includes/config.php'; print_r(json_encode(\->query('DESCRIBE alumnos')->fetchAll(PDO::FETCH_ASSOC)));
