2bdcbf721b84b41bdf3a40f752ea6641d1a9ddc7
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-06-25 10:27:59 +0200

    Fix print button: replace blocked external Wikipedia icon with inline SVG printer

--- comerciales_llamadas.php
+++ comerciales_llamadas.php
@@ -651,7 +651,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && (!empty($_GET['fecha_desde']) || !em
                     <div style="text-align: center; margin-top: 15px;">
                         <button type="submit" class="btn-buscar">Buscar</button>
                         <button type="button" class="btn-print" onclick="window.print()">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width="16" alt="PDF">
+                            <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" style="flex-shrink: 0;"><path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/></svg>
                             Imprimir
                         </button>
                     </div>
