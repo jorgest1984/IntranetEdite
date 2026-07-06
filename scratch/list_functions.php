37b9ebac05ae612d905d0d37ab5ee754e44a713c
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-07-06 13:28:28 +0200

    Add list_functions helper

---
+++ scratch/list_functions.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . '/../includes/moodle_api.php';
+
+try {
+    $moodle = new MoodleAPI($pdo);
+    $info = $moodle->getSiteInfo();
+    echo "SITE INFO SUCCESS!\n";
+    if (isset($info['functions'])) {
+        $funcs = array_map(function($f) { return $f['name']; }, $info['functions']);
+        sort($funcs);
+        echo "Available Functions (" . count($funcs) . "):\n";
+        foreach ($funcs as $fn) {
+            if (strpos($fn, 'course') !== false || strpos($fn, 'enrol') !== false) {
+                echo " - " . $fn . "\n";
+            }
+        }
+    } else {
+        print_r($info);
+    }
+} catch (Exception $e) {
+    echo "ERROR: " . $e->getMessage() . "\n";
+}
