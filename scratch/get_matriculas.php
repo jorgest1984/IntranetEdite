b9b897ceb29a38bc632be21a69e100ada6bf8b99
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-07-07 14:48:10 +0200

    Update get_matriculas.php to dump configuracion table

--- scratch/get_matriculas.php
+++ scratch/get_matriculas.php
@@ -1,4 +1,4 @@
 <?php
 require_once dirname(__DIR__) . '/includes/config.php';
-$stmt = $pdo->query("DESCRIBE matriculas");
+$stmt = $pdo->query("SELECT * FROM configuracion");
 print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
