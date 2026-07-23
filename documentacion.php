7f34a36e513cccafdd4ba083e3eec3ddffaeac06
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-07-23 10:51:49 +0200

    Allow comerciales to access documentacion.php

--- documentacion.php
+++ documentacion.php
@@ -2,7 +2,7 @@
 // documentacion.php
 require_once 'includes/auth.php';
 
-if (!has_permission([ROLE_ADMIN, ROLE_COORD, ROLE_LECTURA, ROLE_TUTOR, ROLE_FORMADOR])) {
+if (!has_permission([ROLE_ADMIN, ROLE_COORD, ROLE_LECTURA, ROLE_TUTOR, ROLE_FORMADOR, ROLE_COMERCIAL])) {
     header("Location: dashboard.php");
     exit();
 }
