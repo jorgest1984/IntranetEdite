75544868979ca8340013bdc985377d3bcd6ae8da
Jorge Salguero <122924163+sisqogr84@users.noreply.github.com>
2026-09-01 11:36:34 +0200

    Enable PHP error reporting for server debugging

--- includes/config.php
+++ includes/config.php
@@ -239,4 +239,10 @@ function audit_log($pdo, $accion, $entidad, $entidad_id = null, $datos_antiguos 
         $agent
     ]);
 }
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ?>
